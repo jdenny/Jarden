b7b3c92452bc54671a3c8627dc7a972ebf55a8e2
jdenny <john.denny@gmail.com>
2015-12-12 14:01:24 +0000

    Removed worksheet src & bin

--- QAScala/.worksheet/src/intro.hello.scala
+++
@@ -1,7 +0,0 @@
-package intro
-
-object hello {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(75); 
-	val welcome = "Welcome to hello Scala world";System.out.println("""welcome  : String = """ + $show(welcome ));$skip(19); 
-  println(welcome);$skip(26); val res$0 = 
-  welcome.groupBy(c => c);System.out.println("""res0: scala.collection.immutable.Map[Char,String] = """ + $show(res$0))}
-}
--- QAScala/src/intro/hello.sc
+++
@@ -1,8 +0,0 @@
-package intro
-
-object hello {
-	val welcome = "Welcome to hello Scala world"
-                                                  //> welcome  : String = Welcome to hello Scala world
-  println(welcome)                                //> Welcome to hello Scala world
-  welcome.groupBy(c => c)
-}
